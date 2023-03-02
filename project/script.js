c611d14bf6bb985caa0573029720bd5ceaa17289
03OCTMWFWD-Piyush <piyushdhunt8980@gmail.com>
2023-03-02 14:58:28 +0530

    Javascript-new

---
+++ project/script.js
@@ -0,0 +1,9 @@
+function Validateform(){
+    var name=document.contactform.name.value;
+    var email=document.contactform.email.value;
+    var mobile=document.contactform.mobile.value;
+    var country=document.contactform.country.value;
+    var gender=document.contactform.gender.value;
+    var hobbies=[];
+    var chekboxes=document.getElementsByName("hobies[]");
+ }
